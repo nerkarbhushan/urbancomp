be889bee41fcdde1492c061bd16f30cf0eb70c82
Bhushan Nerkar <nerkarbhushan.b@gmail.com>
2025-08-29 12:33:43 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -54,7 +54,7 @@ router.delete("/delete-product/:pid", deleteProductController);
 //filter product
 router.post("/product-filters", productFiltersController);
 
-//product count
+//product count || method GEt
 router.get("/product-count", productCountController);
 
 //product per page
